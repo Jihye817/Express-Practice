51e18174be77b19f0954594e719e1b97f05c5984
jihye <wlgul817@gmail.com>
2024-11-25 14:28:28 +0900

    ⚡️ feat : status 코드 추가 및 if문 리팩토링

--- tea-shopping-demo.js
+++ tea-shopping-demo.js
@@ -1,39 +1,27 @@
 //express 모듈 만들기
 const express = require("express");
 const app = express();
+app.use(express.json()); //미들웨어 설정
+app.listen(1234); //포트 설정
 
-//기본 데이터 넣기
-let tea1 = {
-  type: "Green Tea",
-  quantity: 100,
-  bookmarked: 14,
-};
-let tea2 = {
-  type: "Darjeeling",
-  quantity: 70,
-  bookmarked: 34,
-};
-let tea3 = {
-  type: "English Breakfast",
-  quantity: 215,
-  bookmarked: 57,
-};
+/** data format
+ * let tea = {
+ *  tea : "Green Tea",
+ *  quantity : 100,
+ *  bookmarked: 10
+ * }
+ */
 
-let teaList = [tea1, tea2, tea3];
-
-//데이터베이스 역할의 map을 만들어 기존 데이터를 넣어주기
+//데이터베이스 역할의 map을 만들기
 let db = new Map();
-teaList.map((item) => db.set(db.size + 1, item));
 
 //전체 조회
 app.get("/teas", (req, res) => {
-  res.json(Object.fromEntries(db)); //map to json을 하기 위해 Object.formEntries() 사용
-  //다음과 같이 forEach로 사용도 가능
-  // let teas = {};
-  // db.forEach((value, key) => {
-  //   teas[key] = value;
-  // });
-  // res.json(teas);
+  if (db.size != 0) {
+    res.status(200).json(Object.fromEntries(db)); //map to json을 하기 위해 Object.formEntries() 사용
+  } else {
+    res.status(404).json({ message: "등록된 데이터가 없습니다." });
+  }
 });
 
 //개별 조회
@@ -41,21 +29,26 @@ app.get("/teas/:id", (req, res) => {
   let { id } = req.params;
   id = parseInt(id);
   const tea = db.get(id);
-  if (tea === undefined) {
-    res.json({ message: "id가 일치하는 Tea 정보가 없습니다." });
+  if (tea) {
+    res.status(200).json(tea);
   } else {
-    res.json(tea);
+    res.status(404).json({ message: "id가 일치하는 Tea 정보가 없습니다." });
   }
 });
 
 //등록
-app.use(express.json()); //미들웨어 설정
 app.post("/teas", (req, res) => {
   let newTea = req.body;
   db.set(db.size + 1, newTea);
-  res.json({
-    message: `${db.get(db.size).type}가 성공적으로 등록되었습니다.`,
-  });
+  if (req.body.type && req.body.quantity && req.body.bookmarked) {
+    res.status(201).json({
+      message: `${db.get(db.size).type}가 성공적으로 등록되었습니다.`,
+    });
+  } else {
+    res.status(400).json({
+      message: "입력 값을 다시 확인해주세요",
+    });
+  }
 });
 
 //개별 삭제
@@ -63,28 +56,30 @@ app.delete("/teas/:id", (req, res) => {
   let { id } = req.params;
   id = parseInt(id);
   const tea = db.get(id);
-  let msg = "";
-  if (tea == undefined) {
-    msg = "id가 일치하는 Tea 정보가 없습니다.";
-  } else {
+  if (tea) {
     db.delete(id);
-    msg = `${tea.type}의 정보가 삭제되었습니다.`;
+    res.status(200).json({
+      message: `${tea.type}의 정보가 삭제되었습니다.`,
+    });
+  } else {
+    res.status(404).json({
+      message: "id가 일치하는 Tea 정보가 없습니다.",
+    });
   }
-  res.json({ message: msg });
 });
 
 //전체 삭제
 app.delete("/teas", (req, res) => {
-  let msg = "";
-  if (db.size == 0) {
-    msg = "삭제할 데이터가 없습니다.";
-  } else {
+  if (db.size != 0) {
     db.clear(); //map의 모든 객체를 삭제
-    msg = "전체 Tea 데이터가 삭제되었습니다";
+    res.status(200).json({
+      message: "전체 Tea 데이터가 삭제되었습니다",
+    });
+  } else {
+    res.status(404).json({
+      message: "삭제할 데이터가 없습니다.",
+    });
   }
-  res.json({
-    message: msg,
-  });
 });
 
 //개별 수정
@@ -92,10 +87,9 @@ app.put("/teas/:id", (req, res) => {
   let { id } = req.params;
   id = parseInt(id);
   const tea = db.get(id);
+  let statusCode = 200;
 
-  if (tea == undefined) {
-    res.json({ message: "Tea 정보를 찾을 수 없습니다." });
-  } else {
+  if (tea) {
     let oldTea = { ...tea }; //이전 데이터.
     //객체이므로 객체를 복사해준다. let oldTea = tea 이면 같은 데이터를 참조하기 때문에 tea의 내용을 바꾸면 oldTea 내용이 같이 바뀜
     let newTea = req.body; //수정 데이터
@@ -123,16 +117,16 @@ app.put("/teas/:id", (req, res) => {
       oldTea.quantity == newTea.quantity &&
       oldTea.bookmarked == newTea.bookmarked
     ) {
+      statusCode = 404;
       msg = "변경할 데이터가 기존 데이터와 같습니다.";
     }
 
     db.set(id, tea); //id가 일치하는 곳의 객체 수정
 
-    res.json({
+    res.status(statusCode).json({
       message: msg,
     });
+  } else {
+    res.status(404).json({ message: "Tea 정보를 찾을 수 없습니다." });
   }
 });
-
-//포트 설정
-app.listen(1234);
